b221d097df56ea9ec73630609e9bb0c85676314f
tatianaanthony <tatiana.anthony@students.olin.edu>
2014-11-09 00:36:30 -0500

    Keep track of planets generated

--- Assets/SectorMapGenerator.cs
+++ Assets/SectorMapGenerator.cs
@@ -12,9 +12,14 @@ public class SectorMapGenerator : MonoBehaviour {
 	private float prob;
 	private float xchange;
 	private float ychange;	
-	
+	private int totalplanets = 0;
+	private ArrayList locations;
+	//private GameObject currentplanet;
+	//private Vector3 planetlocation;
+
 	// Use this for initialization
 	void Start () { 
+		locations = new ArrayList ();
 		prob = (numberofplanets)/(xsize * ysize / (planetsize *planetsize * 4)); 
 		for (float x = -xsize/2f; x<xsize/2f; x+=planetsize*2) {
 			for (float y=-ysize/2f; y<ysize/2f; y+=planetsize*2) {
@@ -24,6 +29,11 @@ public class SectorMapGenerator : MonoBehaviour {
 					GameObject sector =  (GameObject)GameObject.Instantiate(sector_model,new Vector2(x+xchange,y+ychange),Quaternion.identity);
 					sector.transform.localScale = new Vector3(planetsize,planetsize,planetsize);
 					sector.transform.SetParent(transform);
+					locations.Add(sector);
+					//currentplanet = locations[totalplanets] as GameObject;
+					//planetlocation = currentplanet.transform.position;
+					//print (planetlocation);
+					totalplanets += 1;
 				}
 			}
 		}
