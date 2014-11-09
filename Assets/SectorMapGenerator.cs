502c220a0fe791898a1df2b7a7d8164f1880fe06
tatianaanthony <tatiana.anthony@students.olin.edu>
2014-11-08 20:05:23 -0500

    bigger planets, probability dependent on size of map, size of planet, and number of planets wanted

--- Assets/SectorMapGenerator.cs
+++ Assets/SectorMapGenerator.cs
@@ -5,18 +5,22 @@ public class SectorMapGenerator : MonoBehaviour {
 	
 	public int xsize = 120;
 	public int ysize = 80;
+	public float planetsize = 10f;
+	public int numberofplanets = 10;
 	public GameObject sector_model;
 	
-	private float prob= 0.01f;
+	private float prob;
 	private float xchange;
 	private float ychange;	
 	
 	// Use this for initialization
 	void Start () { 
-		for (int x = -xsize/2; x<xsize/2; x++) {
-			for (int y=-ysize/2; y<ysize/2; y++) {
+		prob = (numberofplanets)/(xsize * ysize / (planetsize *planetsize * 4)); 
+		for (float x = -xsize/2f; x<xsize/2f; x+=planetsize) {
+			for (float y=-ysize/2f; y<ysize/2f; y+=planetsize) {
 				if (Random.Range(0.0f,1.0f)<prob) {
 					GameObject sector =  (GameObject)GameObject.Instantiate(sector_model,new Vector2(x,y),Quaternion.identity);
+					sector.transform.localScale = new Vector3(planetsize,planetsize,planetsize);
 					sector.transform.SetParent(transform);
 				}
 			}
