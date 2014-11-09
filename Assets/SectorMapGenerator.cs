07c03f69036c73630409aca87b1a40d51dd1d8ea
tatianaanthony <tatiana.anthony@students.olin.edu>
2014-11-09 01:05:47 -0500

    Will always make more than a specific number of planets

--- Assets/SectorMapGenerator.cs
+++ Assets/SectorMapGenerator.cs
@@ -14,26 +14,25 @@ public class SectorMapGenerator : MonoBehaviour {
 	private float ychange;	
 	private int totalplanets = 0;
 	private ArrayList locations;
-	//private GameObject currentplanet;
-	//private Vector3 planetlocation;
+
 
 	// Use this for initialization
 	void Start () { 
 		locations = new ArrayList ();
-		prob = (numberofplanets)/(xsize * ysize / (planetsize *planetsize * 4)); 
-		for (float x = -xsize/2f; x<xsize/2f; x+=planetsize*2) {
-			for (float y=-ysize/2f; y<ysize/2f; y+=planetsize*2) {
-				if (Random.Range(0.0f,1.0f)<prob) {
-					xchange = Random.Range (-planetsize/2f, planetsize/2f);
-					ychange = Random.Range (-planetsize/2f, planetsize/2f);
-					GameObject sector =  (GameObject)GameObject.Instantiate(sector_model,new Vector2(x+xchange,y+ychange),Quaternion.identity);
-					sector.transform.localScale = new Vector3(planetsize,planetsize,planetsize);
-					sector.transform.SetParent(transform);
-					locations.Add(sector);
-					//currentplanet = locations[totalplanets] as GameObject;
-					//planetlocation = currentplanet.transform.position;
-					//print (planetlocation);
-					totalplanets += 1;
+		while (totalplanets < numberofplanets){
+			prob = (numberofplanets-totalplanets)/(xsize * ysize / (planetsize *planetsize * 4)); 
+			for (float x = -xsize/2f; x<xsize/2f; x+=planetsize*2) {
+				for (float y=-ysize/2f; y<ysize/2f; y+=planetsize*2) {
+					if (Random.Range(0.0f,1.0f)<prob) {
+						xchange = Random.Range (-planetsize/2f, planetsize/2f);
+						ychange = Random.Range (-planetsize/2f, planetsize/2f);
+						GameObject sector =  (GameObject)GameObject.Instantiate(sector_model,new Vector2(x+xchange,y+ychange),Quaternion.identity);
+						sector.transform.localScale = new Vector3(planetsize,planetsize,planetsize);
+						sector.transform.SetParent(transform);
+						locations.Add(sector);
+						totalplanets += 1;
+
+					}
 				}
 			}
 		}
