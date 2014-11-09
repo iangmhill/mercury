595c3ac275fa0067fdbac1f94bdd0bca0bf3b3b2
unknown <jpflueger@JKNFG12.olin.edu>
2014-11-08 20:08:00 -0500

    added selection script

---
+++ Assets/Resources/Scripts/selector.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class cubeController : MonoBehaviour 
+{
+
+	// Use this for initialization
+	
+	void changeColor()
+	{
+		Color select = new Color (191, 87, 0);
+		Color deSelect = new Color (0, 0, 0);
+		MeshRenderer gameObjectRenderer = gameObject.GetComponent<MeshRenderer> ();
+		Material newMaterial1 = new Material (Shader.Find ("Transparent/Diffuse"));
+		Material newMaterial2 = new Material (Shader.Find ("Transparent/Diffuse"));
+		newMaterial1.color = select;
+		newMaterial2.color = deSelect;
+
+		if (colorChanger) 
+		{
+			gameObjectRenderer.material = newMaterial1;
+			colorChanger = false;
+		}
+		else
+		{
+			gameObjectRenderer.material = newMaterial2;
+			colorChanger = true;
+		}
+
+
+	}
+	void OnMouseDown()
+	{
+		changeColor ();
+	}
+		
+
+
+}
