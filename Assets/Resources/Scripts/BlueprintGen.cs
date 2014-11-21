98e7c49750ca17be9a9e1410555e349a77fc7e12
ManickYoj <nickfrancisci@gmail.com>
2014-11-20 23:46:33 -0500

    Structured Project for Thanksgivings additions.

---
+++ Assets/Resources/Scripts/BlueprintGen.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BlueprintGen : MonoBehaviour {
+	public Texture2D blueprintData;
+	public GameObject blueprintSquare;
+
+	private GameObject[,] blueprint;
+
+	void Start () {
+		blueprint = new GameObject[blueprintData.width, blueprintData.height];
+
+		for (int x = 0; x < blueprintData.width; x++) {
+			for (int y = 0; y < blueprintData.height; y++) {
+				if (Color.black == blueprintData.GetPixel(x, y)) {
+					blueprint[x, y] = (GameObject) GameObject.Instantiate(blueprintSquare, new Vector2(x, y), Quaternion.identity);
+					blueprint[x, y].transform.SetParent(transform);
+				}
+			}
+		}
+	}
+}
